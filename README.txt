6cb1a6aa2f2038142023911985710dcddc125584
Marc Sibson <sibson@gmail.com>
2015-08-07 22:30:01 -0700

    initial webhook to gh issue app

---
+++ README.txt
@@ -0,0 +1,3 @@
+  1. [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/sibson/gheg)
+  2. heroku addons:open postmark -a app-name
+  3. Configure Inbound mail at [Postmark](http://developer.postmarkapp.com/developer-process-configure.html)
---
+++ webapp.py
@@ -0,0 +1,45 @@
+import os
+
+from flask import Flask, request
+
+from github3 import login
+import structlog
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = os.environ.get('DEBUG') in ('1', 'y', 'True', 'true')
+app.config['ghuser'] = os.environ.get('GH_USERNAME')
+app.config['ghpassword'] = os.environ.get('GH_PASSWORD')
+app.config['ghrepo'] = os.environ.get('GH_REPO')
+
+logger = structlog.get_logger()
+
+
+@app.route('/hooks/postmark', methods=['POST'])
+def postmark_incomming_hook():
+    # TODO HTTP Basic Auth
+    inbound = request.json
+
+    logger.debug('postmark', data=inbound)
+
+    title = inbound['subject']
+    body = inbound['TextBody']
+
+    # TODO taskify
+    gh = login(app.config['ghuser'], app.config['ghpassword'])
+    repo = gh.repository(*app.config['ghrepo'].split('/'))
+    repo.create_issue(title, body)
+
+    return 'OK'
+
+if __name__ == '__main__':
+    from structlog.stdlib import LoggerFactory
+    from structlog.threadlocal import wrap_dict
+    import logging
+
+    structlog.configure(
+        context_class=wrap_dict(dict),
+        logger_factory=LoggerFactory(),
+    )
+    logging.basicConfig(level=logging.DEBUG)
+    app.run()
